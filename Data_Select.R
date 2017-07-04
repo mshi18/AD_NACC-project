fc926048d435e5e47cc0f66386e91e149873b695
kosuna1984 <kosuna@mail.lipscomb.edu>
2017-07-03 20:18:26 -0500

    Update Data_Select.R

--- Data_Select.R
+++ Data_Select.R
@@ -84,3 +84,23 @@ Data_Subset_DIABETES[Data_Subset$DIABETES == -4] <- NA
 Data_Subset_CBSTROKE <- factor(Data_Subset$CBSTROKE,levels=c(0:2),labels=c("Absent","Active","Inactive"))
 Data_Subset_CBSTROKE[Data_Subset$CBSTROKE == 9] <- NA
 Data_Subset_CBSTROKE[Data_Subset$CBSTROKE == -4] <- NA
+
+Data_Subset$HYPERTEN <- factor(Data_Subset$HYPERTEN,levels=c(0,1,2),labels=c("Absent","Recent/Active","Remote/Inactive"))
+Data_Subset$HYPERTEN[Data_Subset$HYPERTEN == 9] <- NA
+Data_Subset$HYPERTEN[Data_Subset$HYPERTEN == -4] <- NA                                                                  
+                                                                    
+Data_Subset$DEP2YRS <- factor(Data_Subset$DEP2YRS,levels=c(0,1),labels=c("NO","YES"))
+Data_Subset$DEP2YRS[Data_Subset$DEP2YRS == 9] <- NA
+Data_Subset$DEP2YRS[Data_Subset$DEP2YRS == -4] <- NA
+
+Data_Subset$NACCAPOE <- factor(Data_Subset$NACCAPOE, levels=c(1,2,3,4,5,6),labels=c("e3e3","e3e4","e3e2","e4e4","e4e2","e2e2"))
+Data_Subset$NACCAPOE[Data_Subset$NACCAPOE == 9] <- NA
+Data_Subset$NACCAPOE[Data_Subset$NACCAPOE == -4] <- NA
+                               
+Data_Subset$NACCNE4S <- factor(Data_Subset$NACCNE4S,levels=c(0,1,2),labels=c("No e4 allele","1 copy of e4 allele","2 copies of e4 allele"))
+Data_Subset$NACCNE4S[Data_Subset$NACCNE4S == 9] <- NA
+
+Data_Subset$NPTHAL <- factor(Data_Subset$NPTHAL,levels=c(0,1,2,3,4,5),labels=c("Phase0","Phase1","Phase2","Phase3","Phase4","Phase5"))
+Data_Subset$NPTHAL[Data_Subset$NPTHAL == 8] <- NA
+Data_Subset$NPTHAL[Data_Subset$NPTHAL == 9] <- NA
+Data_Subset$NPTHAL[Data_Subset$NPTHAL == -4] <- NA
